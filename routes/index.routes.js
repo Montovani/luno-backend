bab53a8951cd798c2b909d97e423d2f8249d12a6
Igor Montovani <igor.montovani@gmail.com>
2025-12-03 17:48:41 +0100

    Add Cloudinary route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,11 +4,13 @@ const  userRoutes = require('./user.routes')
 const bookingRoutes = require('./booking.routes')
 const petRoutes = require('./pet.routes')
 const reviewRoutes = require('./review.routes')
+const uploadRoutes = require("./upload.routes");
 
 router.use('/auth', authRoutes)
 router.use('/user',userRoutes)
 router.use('/booking',bookingRoutes)
 router.use('/pet', petRoutes)
 router.use('/review',reviewRoutes)
+router.use("/upload", uploadRoutes);
 
 module.exports = router
